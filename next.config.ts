a97482addc3f37b108bcae9b14431da9dbe7a404
Sokol Djala <sokoldjala@gmail.com>
2025-03-03 00:22:54 +0100

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // This base path should match your GitHub repository name
+  // e.g., if your repo is user/my-project, use '/my-project'
+  basePath: '/virtual-menu',
 };
 
 export default nextConfig;
