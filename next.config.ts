2442de1707c5a0bf388270ff4cf1489f41606e14
kledi25 <sufajkledi25@gmail.com>
2025-03-03 02:00:38 +0100

    path config

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
   },
   // This base path should match your GitHub repository name
   // e.g., if your repo is user/my-project, use '/my-project'
-  basePath: '/virtual-menu',
+  basePath: process.env.NODE_ENV === 'production' ? '/virtual-menu' : '',
 };
 
 export default nextConfig;
